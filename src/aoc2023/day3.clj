ebf5b3efed2d47f32ae2964565d49b4598e1b55b
Stan Verberkt <verberktstan@gmail.com>
2023-12-09 20:13:59 +0100

    Day3a - Combine the numbers, sum the part numbers
    Day3a - Sum the part numbers
    
    Day3a - Tidy up, make it more consise

--- src/aoc2023/day3.clj
+++ src/aoc2023/day3.clj
@@ -1,59 +1,60 @@
 (ns aoc2023.day3
-  (:require [clojure.string :as str]))
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-(def TESTINPUT
-  "467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..")
-
-(defn- pivot
-  "Returns a map with characters from the lines keyed by their [row-index column-index].
-  eg: `(pivot ['ab' 'cd']) => {[0 0] a, [0 1] b, [1 0] c, [1 1] d}`"
+(defn- position-pairs
+  "Returns a map with the characters keyed by their x/y position.
+  eg: `(position-pairs 'ab\ncd') => {[0 0] a, [0 1] b, [1 0] c, [1 1] d}`"
   [lines]
-  (apply
-   merge
-   (for [row-idx (-> lines count (range)) ; for every row..
-         col-idx (-> lines first count (range))] ; ..and every col
-     (let [row       (nth lines row-idx)
-           character (nth row   col-idx)]
-       {[row-idx col-idx] character})))) ; save the car by row- and column-index.
+  (->> (for [row (-> lines count (range))
+             col (-> lines first count (range))]
+         (let [position [row col]
+               character (-> (map seq lines) (nth row) (nth col))]
+           [position character]))
+       (into {})))
+
+(defn- number-char? [character]
+  (try (-> character str edn/read-string number?) (catch Exception _ nil)))
+
+(defn- combine-numbers*
+  "Returns sequence `coll` with a map conjoined with the combined :number and
+  the :positions of the individual digits in the grid."
+  [coll position-pairs]
+  (cond-> coll
+    (every? (comp number-char? second) position-pairs)
+    (conj {:number    (apply (comp edn/read-string str) (map second position-pairs))
+           :positions (map first position-pairs)})))
+
+(def combine-numbers (partial reduce combine-numbers* nil))
 
 (def DIRECTIONS
-  {:north     [-1 0] :northeast [-1 1] :east [0 1]
-   :southeast [1 1]  :south     [1 0]  :southwest [1 -1]
-   :west      [0 -1] :northwest [-1 -1]})
-
-(defn- adjacent-characters
-  "Returns a sequence of maps, one for each coordinate, with an adjacent symbol associated with it's :direction"
-  [pivoted-map]
-  (for [[k] pivoted-map]
-    (reduce-kv
-     (fn [m direction pos]
-       (let [pos'       (mapv + k pos)
-             adjacent   (get pivoted-map pos')
-             symbol-map {:pos pos' :symbol adjacent :direction direction}]
-         (cond-> m
-           adjacent (update :adjacent conj symbol-map))))
-     {:pos k}
-     DIRECTIONS)))
-
-(def DOT-OR-INT? #{\. \0 \1 \2 \3 \4 \5 \6 \7 \8 \9})
+  [[0 -1] [1 -1] [1 0] [1 1] [0 1] [-1 1] [-1 0] [-1 -1]])
 
 (defn- part-number?
-  "Returns a truethy value if one of the adjacent symbols is not a dot or an int."
-  [{:keys [adjacent]}]
-  (some (complement DOT-OR-INT?) (map :symbol adjacent)))
+  "Returns a function that returns the number when it is adjacent to a symbol."
+  [lookup]
+  (fn part-number*
+    [{:keys [positions number]}]
+    (when (->> positions
+               (mapcat #(map (comp lookup (partial mapv + %)) DIRECTIONS))
+               (filter identity)
+               (some (complement #{nil \. \0 \1 \2 \3 \4 \5 \6 \7 \8 \9})))
+      number)))
+
+(defn- collect-numbers [lookup]
+  (->> lookup
+       (sort-by key)
+       (partition-by (comp number-char? second))
+       (partition-all 2)
+       (mapcat combine-numbers)))
+
+(defn- sum [predicate lookup numbers]
+  (->> numbers (keep (predicate lookup)) (reduce +)))
 
 (comment
-  (let [lines (str/split TESTINPUT #"\n")]
-    (->> lines
-         pivot
-         adjacent-characters
-         (filter part-number?))))
+  (with-open [reader (io/reader "resources/day3a-input.txt")]
+    (let [lookup (->> reader line-seq position-pairs)
+          numbers (collect-numbers lookup)]
+      (sum part-number? lookup numbers))) ; 512794
+  )
