c6b0b2e6653e73fbfa37a7407fc765cdda26f57c
Stan Verberkt <verberktstan@gmail.com>
2023-12-12 20:20:14 +0100

    Day5a - Parse almanac lines' seeds, mapping and ranges

--- src/aoc2023/day5.clj
+++ src/aoc2023/day5.clj
@@ -1,21 +1,35 @@
 (ns aoc2023.day5
   (:require [aoc2023.day1 :refer [with-lines]]
             [clojure.edn :as edn]
-            [clojure.set :as set]
-            [clojure.test :as t]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.test :as t]))
+
+(def SEED? #"seeds\:[ \d]+")
+(def MAPPING? #"[\S]+\-to\-[\S]+")
+
+(defn- parse-ints [s]
+  (some->> s (re-seq #"\d+") (map edn/read-string)))
 
 (defn- parse-line [s]
-  (let [seeds (some->> s (re-find #"seeds\:[ \d]+") (re-seq #"\d+") (map edn/read-string))
-        mapping (some->> s (re-find #"[\S]+\-to\-[\S]+") #_(re-seq #"[\S]+\-to|to\-[\S]+"))
-        ranges (some->> s (re-seq #"\d+") (map edn/read-string))]
-    (if (or seeds mapping)
-      {:seeds seeds
-       :mapping (some-> mapping (str/split #"-to-"))}
-      (zipmap [:destination-start :source-start :length] ranges))))
+  (let [seeds (some->> s (re-find SEED?) parse-ints)
+        mapping (some->> s (re-find MAPPING?))
+        ranges (parse-ints s)]
+    (cond
+      seeds   {:seeds seeds}
+      mapping {:mapping (some-> mapping (str/split #"-to-"))}
+      ranges  (zipmap [:destination-start :source-start :length] ranges))))
+
+(defn- combine-mappings* [{:keys [mapping] :as result} {:keys [seeds length] :as m}]
+  (cond
+    seeds                m
+    (and mapping length) (assoc result mapping m)
+    (:mapping m)         (merge result m)
+    :else                result))
+
+(def combine-mappings (partial reduce combine-mappings* {}))
 
 (comment
-  (with-lines "resources/day5-testinput.txt" (partial mapv parse-line)))
+  (with-lines "resources/day5-testinput.txt" (comp combine-mappings (partial keep parse-line))))
 
 (t/deftest day5a
   (t/are [result file] (= result (-> file (with-lines count)))
